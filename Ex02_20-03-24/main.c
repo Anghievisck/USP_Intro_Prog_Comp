6495e646efc0aa7077f47c6b73b267175c255817
Anghievisck <pedro.luis.angh@gmail.com>
2024-03-20 15:50:49 -0300

    Exercício 2 concluido

---
+++ Ex02_20-03-24/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+    float priceTortilla, priceTejuine, priceManzapane, currentMoney;
+
+    scanf("%f %f %f %f", &priceTortilla, &priceTejuine, &priceManzapane, &currentMoney);
+
+    if(currentMoney < priceTortilla){
+        printf("Dikson nao pode comprar nada");
+    } else if(currentMoney >= (priceTortilla + priceTejuine + priceManzapane)){
+        printf("Dikson pode comprar tortilla, tejuine e manzapane");
+    } else if(currentMoney >= (priceTortilla + priceTejuine)){
+        printf("Dikson pode comprar tortilla e tejuine");
+    } else if(currentMoney >= priceTortilla){
+        printf("Dikson pode comprar tortilla");
+    }
+}
